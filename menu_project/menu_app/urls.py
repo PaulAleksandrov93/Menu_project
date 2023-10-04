42fcce6b382742d95d8e8569fde0c79affb42e06
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-10-04 23:06:39 +0500

    some fixes in views

--- menu_project/menu_app/urls.py
+++ menu_project/menu_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
-    # path('about/', views.about, name='about'),
+    path('home/<str:menu_name>/', views.home, name='home'),
+    path('about/<str:menu_name>/', views.about, name='about'),
 ]
--- menu_project/menu_app/views.py
+++ menu_project/menu_app/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from .models import MenuItem
 
-def home(request):
-    menu_items = MenuItem.objects.values_list('title', flat=True)
+def get_menu(menu_name):
+    return MenuItem.objects.filter(menu_name=menu_name, parent__isnull=True)
+
+
+def home(request, menu_name):
+    menu_items = get_menu(menu_name)
     context = {
         'menu_items': menu_items,
         'active_url': request.path
     }
     return render(request, 'menu_app/home.html', context)
 
-
-# def about(request, menu_name):
-#     menu_items = get_menu(menu_name)
-#     context = {
-#         'menu_items': menu_items,
-#         'active_url': request.path
-#     }
-#     return render(request, 'menu_app/about.html', context)
+def about(request, menu_name):
+    menu_items = get_menu(menu_name)
+    context = {
+        'menu_items': menu_items,
+        'active_url': request.path
+    }
+    return render(request, 'menu_app/about.html', context)
